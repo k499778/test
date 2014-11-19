d9237e888c93a6d6fadee3402a48ce0b9fe12ee7
Ryohei Takegawa <takegawa@dotinstall.com>
2014-11-19 11:03:23 +0900

    Revert "commit 4"
    This reverts commit 8ab72a9a43f5ce2db08554f5825f490a7a6ba5af.

--- src/aaaaaaaaaa.java
+++ src/aaaaaaaaaa.java
@@ -4,8 +4,6 @@ public class aaaaaaaaaa {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 System.out.println("おっしゃー！！Ｌ(●´ω｀●)");
-System.out.println("からの～おっしゃー！！Ｌ(●´ω｀●)");
-
 	}
 
 }
