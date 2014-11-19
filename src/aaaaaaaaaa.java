8ab72a9a43f5ce2db08554f5825f490a7a6ba5af
Ryohei Takegawa <takegawa@dotinstall.com>
2014-11-19 11:00:37 +0900

    commit 4

--- src/aaaaaaaaaa.java
+++ src/aaaaaaaaaa.java
@@ -4,6 +4,8 @@ public class aaaaaaaaaa {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 System.out.println("おっしゃー！！Ｌ(●´ω｀●)");
+System.out.println("からの～おっしゃー！！Ｌ(●´ω｀●)");
+
 	}
 
 }
