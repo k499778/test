7b9d6d4224ac7fd4e5679a9fe4aee22351524036
Ryohei Takegawa <takegawa@dotinstall.com>
2014-11-20 15:40:42 +0900

    "攻め"あるのみ

--- src/aaaaaaaaaa.java
+++ src/aaaaaaaaaa.java
@@ -4,6 +4,8 @@ public class aaaaaaaaaa {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 System.out.println("おっしゃー！！Ｌ(●´ω｀●)ポイズン");
+System.out.println("おっしゃー！！Ｌ(●´ω｀●)ポイズン");
+System.out.println("おっしゃー！！Ｌ(●´ω｀●)ポイズン");
 	}
 
 }
